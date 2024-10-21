136554177afd59a6bee5e9846b6440d4a767ba7d
evan <evanjonedwards@gmail.com>
2024-10-21 10:11:37 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,70 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: eedwards <eedwards@student.hive.fi>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/08/12 13:17:22 by eedwards          #+#    #+#              #
+#    Updated: 2024/10/17 13:50:05 by eedwards         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Project name and compiler
+NAME = philo
+CC = cc -g
+
+# Compiler and linker flags
+CFLAGS = -Wall -Werror -Wextra -Ofast
+LFLAGS = -L./$(LIB1) -l$(LIB1_NAME) -L./$(LIB2) -l$(LIB2_NAME)
+
+# Libraries
+LIB1 = lib1_directory
+LIB1_NAME = lib1_name
+LIB2 = lib2_directory
+LIB2_NAME = lib2_name
+
+# Directories
+OBJ_DIR = obj
+SRC_DIR = src
+INC_DIR = incl
+
+# Include paths
+INC = -I./$(LIB1) -I./$(LIB2) -I./$(INC_DIR)
+
+# Source files
+SOURCES = input_validation.c \
+          ph_init.c \
+          ph_main.c \
+		  threads.c \
+		  utils.c
+
+# Object files
+SRCS = $(addprefix $(SRC_DIR)/, $(SOURCES))
+OBJS = $(addprefix $(OBJ_DIR)/, $(SOURCES:.c=.o))
+
+# Rules
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) $(INC) -c $< -o $@
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@make -C $(LIB1)
+	@make -C $(LIB2)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(LFLAGS)
+
+clean:
+	$(RM) $(OBJS)
+	@make -C $(LIB1) clean
+	@make -C $(LIB2) clean
+
+fclean: clean
+	$(RM) $(NAME)
+	make -C $(LIB1) fclean
+	make -C $(LIB2) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ replace_env.c
@@ -0,0 +1,248 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace_env.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eedwards <eedwards@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 20:52:11 by ttero             #+#    #+#             */
+/*   Updated: 2024/10/21 10:00:56 by eedwards         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct	s_token
+{
+	char			*str;
+	int				type;
+	struct s_token	*prev;
+	struct s_token	*next;
+}				t_token;
+
+typedef struct s_mini
+{
+    int     status;
+}					t_mini;
+
+typedef enum e_status
+{
+	DEFAULT,
+	SINGLEQ,
+	DOUBLEQ,
+}					t_status;
+
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
+
+bool isDelimiter(char ch)
+{
+    if (ch == ' ' || ch == '<' || ch == '>' || ch == '*' )
+        return (true);
+    return (false);
+}
+
+int is_quotes (char *s, int *i)
+{
+    if (s[*i] == '\'' || (s[*i] == '\"'))
+        return (1);
+    return (0);
+}
+
+int quotes(char *s, int *i, t_mini mini)
+{
+	if (s[*i] == '\'')
+	{
+		if (mini.status == DEFAULT)
+		{
+			mini.status == SINGLEQ;
+			*i ++;
+		}
+		else if (mini.status == SINGLEQ)
+		{
+			mini.status == DEFAULT;
+			*i ++;
+		}
+		else
+		{
+			return (1);
+            //mini.status[0] == s[*i];
+			//i++;
+		}
+        if (is_quotes(s, i) == 1)
+            quotes(s, i, mini);
+		return (1);
+	}
+	if (s[*i] == '\"')
+	{
+		if (mini.status == DEFAULT)
+		{
+			mini.status == DOUBLEQ;
+			*i ++;
+		}
+		else if (mini.status == DOUBLEQ)
+		{
+			mini.status == DEFAULT;
+			*i ++;
+		}
+		else
+		{
+			return (1);
+            //mini.token[0] == s[*i];
+			//i++;
+		}
+        if (is_quotes(s, i) == 1)
+            quotes(s, i, mini);
+		return (1);
+	}
+	return (0);
+}
+
+
+
+char  *add_mem (char *str, int add, int old)
+{
+    char *copy;
+
+	copy  = strdup(str);
+    free(str);
+    str = malloc(old + add);
+    str = strcpy(str, copy);
+    free (copy);
+    add = old + add;
+    return (str);
+}
+
+char *add_end (char *dst, char *add, int j)
+{
+    int i;
+
+    i = 0;
+    //j++;
+	printf("%s\n", add);
+	//printf("%s\n", copy);
+    while (add[i])
+    {
+        dst[j] = add[i];
+        j++;
+        i++;
+    }
+	dst[j] = '\0';
+	printf("%s\n", dst);
+    return (dst);
+}
+
+char *get_env (char *str, int i, char **env)
+{
+    char *search;
+    int j;
+    char *ret;
+
+	//return (strdup(""));
+	if (ft_isdigit(str[i + 1]) == 1)
+        return (strdup(""));
+    j = 0;
+    i++;
+	//return (strdup(""));
+    while(str[i] && isDelimiter(str[i]) == false)
+    {
+        i++;
+        j++;
+    }
+	//return (strdup(""));
+    search = malloc(j + 1);
+    i -= j;
+    j = 0;
+    while(str[i] && isDelimiter(str[i]) == false)
+    {
+        search[j] = str[i];
+        j++;
+        i++;
+    }
+    search[j] = '\0';
+    i = 0;
+    while (env[i])
+    {
+		if (strncmp(env[i], search, j) == 0 && env[i][j] == '=')
+			break;
+		i++;
+    }
+	if (env[i] == NULL)
+		return (strdup(""));
+    ret = strchr(env[i], '=');
+    return (ret);
+
+}
+
+
+
+char *first_check(char *str, char **env,  t_mini mini)
+{
+    int i;
+    char *env_var;
+    char *copy;
+    int j;
+    int mem;
+	//t_mini mini;
+
+
+	i = 0;
+    j = 0;
+    mem = strlen(str);
+	printf("%d\n", mem);
+	//return (NULL);
+    copy = malloc(mem + 1);
+	//return (NULL);
+	//return (NULL);
+    while (str[i])
+    {
+        quotes(str, &i, mini);
+        if (str[i] == '$' && mini.status != SINGLEQ)
+        {
+			env_var = get_env(str, i, env);
+			//printf("%s\n", env1);
+			//return (NULL);
+            copy = add_mem(copy, strlen(env_var), mem);
+			//return (NULL);
+            mem += strlen(env_var);
+            copy = add_end(copy, env_var, j);
+            //free (env1);
+			return (copy);
+            i += strlen(env_var);   //fix
+            j += strlen(env_var);
+        }
+        else
+        {
+			copy[j] = str[i];
+            j++;
+            i++;
+        }
+    }
+	copy[j] = '\0';
+    if (mini.status != DEFAULT)
+        printf("uneven quotes");
+	return (copy);
+}
+
+
+int	main(int argc, char *argv[], char **envp)
+{
+	char *res;
+	t_mini mini;
+
+	res = first_check(argv[1], envp, mini);
+    printf(" %s\n", res);
+
+	return (1);
+}
+
